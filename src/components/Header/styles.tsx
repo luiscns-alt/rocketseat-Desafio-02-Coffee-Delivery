9bd4ebda86642dc79a7893ef0063449b0fa06c69
Luis Carlos <luiscarlosnors@gmail.com>
2022-09-03 13:36:58 -0300

    feat: top fix header.

--- src/components/Header/styles.tsx
+++ src/components/Header/styles.tsx
@@ -4,6 +4,10 @@ export const HeaderContainer = styled.header`
   width: 100%;
   height: 10.4rem;
   background: ${({ theme }) => theme.colors['--background']};
+  position: sticky;
+  top: 0;
+  left: 0;
+  z-index: 5;
 `
 
 export const HeaderContent = styled.div`
@@ -38,6 +42,7 @@ export const HeaderButton = styled.button<HeaderButtonProps>`
   padding: 0 1.2rem;
   position: relative;
   cursor: inherit;
+
   span {
     position: absolute;
     width: 1.25rem;
@@ -52,19 +57,21 @@ export const HeaderButton = styled.button<HeaderButtonProps>`
     font-size: 1.4rem;
     font-weight: 400;
   }
+
   font-size: ${({ theme }) => theme.textSizes['--text-regular-S']};
   ${({ variant }) => css`
     background: ${({ theme }) => theme.colors[`--${variant}-light`]};
     color: ${({ theme }) => theme.colors[`--${variant}-dark`]};
+
     span {
       background: ${({ theme }) => theme.colors[`--${variant}-dark`]};
     }
   `}
   ${({ variant }) =>
-    variant === 'purple' &&
-    css`
-      svg {
-        color: ${({ theme }) => theme.colors[`--${variant}`]};
-      }
-    `}
+          variant === 'purple' &&
+          css`
+            svg {
+              color: ${({ theme }) => theme.colors[`--${variant}`]};
+            }
+          `}
 `
